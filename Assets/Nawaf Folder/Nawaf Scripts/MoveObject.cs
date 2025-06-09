f8efac584eb1dcb23449952b63836be74621b5dd
TheNawaf5 <Jacob5355355@gmail.com>
2025-06-09 19:04:57 +0300

    Main Menu

---
+++ Assets/Nawaf Folder/Nawaf Scripts/MoveObject.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MoveObject : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        animator.SetTrigger("Hover");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        animator.SetTrigger("Idle"); // Or use a bool if you want smooth transitions
+    }
+}
